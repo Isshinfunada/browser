b29d332cbf4f4dbed918c320714f3f7d8e82e96e
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2026-01-17 09:41:33 +0900

    Url構造体にHTTPスキームのテストケースを追加し、サポートされていないスキームに対するエラーハンドリングを検証

--- saba/src/saba_core/src/url.rs
+++ saba/src/saba_core/src/url.rs
@@ -190,3 +190,17 @@ fn test_url_host_port_path_searchquery() {
     });
     assert_eq!(expected, Url::new(url).parse());
 }
+
+#[test]
+fn test_no_scheme() {
+    let url = "example.com".to_string();
+    let expected = Err("Only HTTP scheme is supported".to_string());
+    assert_eq!(expected, Url::new(url).parse());
+}
+
+#[test]
+fn test_unsupported_scheme() {
+    let url = "https://example.com:8888/index.html".to_string();
+    let expected = Err("Only HTTP scheme is supported".to_string());
+    assert_eq!(expected, Url::new(url).parse());
+}
