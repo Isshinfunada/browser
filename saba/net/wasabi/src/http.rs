15705b50a30dfd9450c4d19bfe7f76f614ba4be2
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2026-01-25 13:41:09 +0900

    HttpClient構造体を追加し、HTTPリクエストの送信機能を実装

---
+++ saba/net/wasabi/src/http.rs
@@ -0,0 +1,68 @@
+pub struct HttpClient {}
+
+extern crate alloc;
+use core::f32::consts::E;
+
+use alloc::format;
+use alloc::string::String;
+use alloc::string::ToString;
+use noli::net::lookup_host;
+use noli::net::SocketAddr;
+use noli::net::TcpStream;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
+
+impl HttpClient {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        let ips = match lookup_host(&"example.com") {
+            Ok(ips) => ips,
+            Err(_) => {
+                return Err(Error::Network(format!(
+                    "Failed to find IP addresses: {:#?}",
+                    e
+                )));
+            }
+        };
+
+        if ips.len() < 1 {
+            return Err(Error::Network(
+                "No IP addresses found for the given host".to_string(),
+            ));
+        }
+
+        let socket_addr = SocketAddr::new(ips[0], port).into();
+
+        let mut stream = match TcpStream::connect(socket_addr) {
+            Ok(stream) => stream,
+            Err(_) => {
+                return Err(Error::Network((
+                    "Failed to connect to TCP stream".to_string(),
+                )));
+            }
+        };
+
+        let mut request = String::from("GET /");
+        request.push_str(&path);
+        request.push_str(" HTTP/1.1\n");
+
+        // ヘッダの追加
+        request.push_str("Host: ");
+        request.push_str(&host);
+        request.push_str("\n");
+        request.push_str("Accept: text/html\n");
+        request.push_str("Connection: close\n");
+        request.push_str("\n");
+
+        let _bytes_written = match stream.write(request.as_bytes()) {
+            Ok(bytes) => bytes,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to send a request to TCP stream".to_string(),
+                ));
+        }
+    }
+}
