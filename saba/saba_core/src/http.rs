28c2555d24a284fa5736e6064cbba9cc9751d73c
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2026-01-26 10:07:46 +0900

    HttpResponseおよびHeader構造体を追加し、HTTPレスポンスの基本構造を実装

---
+++ saba/saba_core/src/http.rs
@@ -0,0 +1,22 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone)]
+pub struct Header {
+    name: String,
+    value: String,
+}
+
+impl Header {
+    pub fn new(name: String, value: String) -> Self {
+        Self { name, value }
+    }
+}
+
+pub struct HttpResponse {
+    version: String,
+    status_code: u32,
+    reason: String,
+    headers: Vec<Header>,
+    body: String,
+}
