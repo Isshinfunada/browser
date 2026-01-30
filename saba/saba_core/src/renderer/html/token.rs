b9fa1b18c0d89eada2249fe74edf59dbda1d7dce
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2026-01-30 10:20:11 +0900

    ScriptDataEndTagName

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -488,6 +488,23 @@ impl Iterator for HtmlTokenizer {
                     return Some(HtmlToken::Char('<'));
                 }
 
+                State::ScriptDataEndTagName => {
+                    if c == '>' {
+                        self.state = State::Data;
+                        return self.take_latest_token();
+                    }
+                    if c.is_ascii_alphabetic() {
+                        self.buf.push(c);
+                        self.append_tag_name(c.to_ascii_lowercase());
+                        continue;
+                    }
+
+                    self.state = State::TemporaryBuffer;
+                    self.buf = String::from("</") + &self.buf;
+                    self.buf.push(c);
+                    continue;
+                }
+
                 _ => {}
             }
         }
